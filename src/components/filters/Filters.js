a70d2e82efdeb8e98a61d32ca84d2bf67c9edd2d
annapismenna <pismennaanna@gmail.com>
2018-01-29 14:55:42 +0700

    save items2

--- src/components/filters/Filters.js
+++ src/components/filters/Filters.js
@@ -14,19 +14,23 @@ class Filters extends Component {
 
         return (
             <div className='Filter-container'>
-                <div className='items-left'>{ `${falses.length} items left` }</div>
-                <div className='filter-button'>
-                    {/* если active && completed - false, не выбраны фильтры и показываем все. */}
-                    {/* назначаем кнопке All класс active */}
-                    <button className={ (!filters.active && !filters.completed) ? 'active' : '' } onClick={this.props.filterAll}> All </button>
-                    {/* назначаем кнопке Active класс active */}
-                    <button className={ filters.active ? 'active' : '' } onClick={this.props.filterActive}> Active </button>
-                    {/* назначаем кнопке completed класс active */}
-                    <button className={ filters.completed ? 'active' : '' } onClick={this.props.filterCompleted}> Completed </button>
+                <div className='main-layout'>
+                    <div className='items-left'>{ `${falses.length} items left` }</div>
+                    <div className='filter-button'>
+                        {/* если active && completed - false, не выбраны фильтры и показываем все. */}
+                        {/* назначаем кнопке All класс active */}
+                        <button className={ (!filters.active && !filters.completed) ? 'active' : '' } onClick={this.props.filterAll}> All </button>
+                        {/* назначаем кнопке Active класс active */}
+                        <button className={ filters.active ? 'active' : '' } onClick={this.props.filterActive}> Active </button>
+                        {/* назначаем кнопке completed класс active */}
+                        <button className={ filters.completed ? 'active' : '' } onClick={this.props.filterCompleted}> Completed </button>
+                    </div>
+                    <div className='filter-clear-completed'>
+                        <button onClick={this.props.clearCompleted}> Clear completed </button>
+                    </div>
                 </div>
-                <div className='filter-clear-completed'>
-                    <button onClick={this.props.clearCompleted}> Clear completed </button>
-                </div>
+                <div className='filter-layout1'></div>
+                <div className='filter-layout2'></div>
             </div>
         );
     }
